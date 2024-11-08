e0428aefe080a792590c63905937d4be9280abb3
JoCode-dev <46027270+JoCode-dev@users.noreply.github.com>
2024-11-08 20:05:30 +0000

    feat: Integrated Redux

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,16 +8,21 @@ export const CartSlice = createSlice({
   reducers: {
     addItem: (state, action) => {
       const existingItem = state.items.find(
-        (item) => item.id === action.payload.id
+        (item) => item.name === action.payload.name
       );
       if (existingItem) {
         existingItem.quantity++;
       } else {
-        state.items.push({ ...action.payload, quantity: 1 });
+        state.items.push({
+          name: action.payload.name,
+          image: action.payload.image,
+          cost: action.payload.cost,
+          quantity: 1,
+        });
       }
     },
     removeItem: (state, action) => {
-      state.items = state.items.filter((item) => item.id !== action.payload);
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
       const { name, quantity } = action.payload;
